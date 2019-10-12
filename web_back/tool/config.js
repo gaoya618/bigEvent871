70743acd6591621b022829567cb0833710cdc02c
gaoya <lll@qq.com>
2019-10-12 17:06:50 +0800

    dav
    大V

---
+++ web_back/tool/config.js
@@ -0,0 +1,28 @@
+// config - 中文含义为配置
+// - 我们可以将项目中使用的接口地址统一保存在这个文件中
+
+var baseUrl = 'http://localhost:8000';
+
+var USER_LOGIN = baseUrl + '/admin/login';
+var USER_LOGOUT = baseUrl + '/admin/logout';
+var USER_INFO = baseUrl + '/admin/login';
+var USER_GETUSER = baseUrl + '/admin/getuser';
+var USER_ARTICLE = baseUrl + '/admin/article_count';
+var USER_COMMENT = baseUrl + '/admin/comment_count';
+var USER_INFO = baseUrl + '/admin/month_article_count';
+var USER_INFO = baseUrl + '/admin/article_category_count';
+var USER_INFO = baseUrl + '/admin/article_category_visit';
+var USER_INFO = baseUrl + '/admin/search';
+var USER_INFO = baseUrl + '/admin/article_publish';
+var USER_INFO = baseUrl + 'http://localhost:8000/admin/article_edit';
+var USER_INFO = baseUrl + '/admin/article_delete';
+var USER_INFO = baseUrl + '/admin/category_search';
+var USER_INFO = baseUrl + '/admin/category_add';
+var USER_INFO = baseUrl + '/admin/category_edit';
+var USER_INFO = baseUrl + '/admin/category_delete';
+var USER_INFO = baseUrl + '/admin/comment_search';
+var USER_INFO = baseUrl + '/admin/comment_pass';
+var USER_COMREJ = baseUrl + '/admin/comment_reject';
+var USER_COMDEL = baseUrl + '/admin/comment_delete';
+var USER_INFOGET = baseUrl + '/admin/userinfo_get';
+var USER_INFOEDIT = baseUrl + '/admin/userinfo_edit';
